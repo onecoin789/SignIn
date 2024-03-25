7f86e063031cfd7cba80cb1ab35ef3ce9969587c
onecoin789 <one_coin@naver.com>
2024-03-25 12:50:54 +0900

    기능추가(아이디/비밀번호 저장)

--- app/src/main/java/com/example/signinapp/SignInActivity.kt
+++ app/src/main/java/com/example/signinapp/SignInActivity.kt
@@ -13,29 +13,34 @@ import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var edit_id: EditText
+    private lateinit var edit_password: EditText
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-        val id = findViewById<EditText>(R.id.edit_id)
-        val password = findViewById<EditText>(R.id.edit_password)
+        edit_id = findViewById(R.id.edit_id)
+        edit_password = findViewById(R.id.edit_password)
         val btn_in1 = findViewById<Button>(R.id.btn_in1)
         val btn_in2 = findViewById<Button>(R.id.btn_in2)
+
+        setResultNext()
 
         //btn_in1(로그인 버튼)을 눌렀을 때
         btn_in1.setOnClickListener {
             //아이디 입력 EditText가 비어있을 때
-            if (id.text.toString().trim().isEmpty()) {
+            if (edit_id.text.toString().trim().isEmpty()) {
                 Toast.makeText(this@SignInActivity, "아이디를 확인해주세요", Toast.LENGTH_SHORT).show()
                 //비밀번호 입력 EditText가 비어있을 때
-            } else if (password.text.toString().trim().isEmpty()) {
+            } else if (edit_password.text.toString().trim().isEmpty()) {
                 Toast.makeText(this@SignInActivity, "비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show()
                 //성공적으로 로그인이 되면 HomeActivity로 go!
             } else {
                 Toast.makeText(this@SignInActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, HomeActivity::class.java)
                 //val inputId는 EditText id에서 받아온 text를 toString으로 바꿔서
-                val inputId = id.text.toString()
+                val inputId = edit_id.text.toString()
                 //intent 보내준다
                 intent.putExtra("", inputId)
                 startActivity(intent)
@@ -44,7 +49,22 @@ class SignInActivity : AppCompatActivity() {
         //btn_in2(회원가입 버튼)을 눌렀을 때 ShignUpActivity로 go!
         btn_in2.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+            resultLauncher.launch(intent)
         }
     }
+    private fun setResultNext() {
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val data: Intent? = result.data
+                    data?.let {
+                        val id = result.data?.getStringExtra("id") ?: ""
+                        val password = result.data?.getStringExtra("password") ?: ""
+                        edit_id.setText(id)
+                        edit_password.setText(password)
+                    }
+                }
+            }
+    }
 }
+
--- app/src/main/java/com/example/signinapp/SignUpActivity.kt
+++ app/src/main/java/com/example/signinapp/SignUpActivity.kt
@@ -9,14 +9,20 @@ import android.widget.EditText
 import android.widget.Toast
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var edit1:EditText
+    private lateinit var edit2:EditText
+    private lateinit var edit3:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+
+
         val btn_up = findViewById<Button>(R.id.btn_up)
-        val edit1 = findViewById<EditText>(R.id.edit1)
-        val edit2 = findViewById<EditText>(R.id.edit2)
-        val edit3 = findViewById<EditText>(R.id.edit3)
+        edit1 = findViewById(R.id.edit1)
+        edit2 = findViewById(R.id.edit2)
+        edit3 = findViewById(R.id.edit3)
 
         //btn_up(회원가입 버튼)을 눌렀을 때 각 EditText가 비어있다면 Toast 메세지 출력과 함께 넘어가지 않는다.
         btn_up.setOnClickListener {
@@ -30,8 +36,15 @@ class SignUpActivity : AppCompatActivity() {
             //3가지 EditText가 전부 제대로 입력되었다면 Toast 메세지와 함께 현재화면이 종료된다.
             else {
                 Toast.makeText(this@SignUpActivity, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
-                finish()
+                signUp()
             }
         }
+    }
+    private fun signUp() {
+        val intent = Intent(this@SignUpActivity, SignInActivity::class.java)
+        intent.putExtra("id", edit2.text.toString())
+        intent.putExtra("password", edit3.text.toString())
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }
